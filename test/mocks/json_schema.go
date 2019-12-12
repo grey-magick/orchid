ac76be1b615b3fa9f3d26a7b96a091d412ee0200
Otávio Fernandes <otaviof@gmail.com>
2019-12-12 19:00:01 +0100

    Mocks including required fields.

--- test/mocks/json_schema.go
+++ test/mocks/json_schema.go
@@ -7,12 +7,14 @@ import (
 func JSONSchemaProps(
 	jsonSchemaType string,
 	format string,
+	required []string,
 	properties map[string]extv1beta1.JSONSchemaProps,
 ) extv1beta1.JSONSchemaProps {
 	return extv1beta1.JSONSchemaProps{
 		Type:       jsonSchemaType,
 		Format:     format,
 		Properties: properties,
+		Required:   required,
 	}
 }
 
@@ -20,19 +22,19 @@ func JSONSchemaProps(
 func OpenAPIV3SchemaMock() extv1beta1.JSONSchemaProps {
 	return extv1beta1.JSONSchemaProps{
 		Properties: map[string]extv1beta1.JSONSchemaProps{
-			"apiVersion": JSONSchemaProps("string", "", nil),
-			"kind":       JSONSchemaProps("string", "", nil),
-			"metadata":   JSONSchemaProps("object", "", nil),
-			"spec": JSONSchemaProps("object", "", map[string]extv1beta1.JSONSchemaProps{
-				"simple": JSONSchemaProps("integer", "int32", nil),
-				"complex": JSONSchemaProps("object", "", map[string]extv1beta1.JSONSchemaProps{
-					"simple_nested": JSONSchemaProps("integer", "int32", nil),
-					"complex_nested": JSONSchemaProps("object", "", map[string]extv1beta1.JSONSchemaProps{
-						"attribute": JSONSchemaProps("string", "byte", nil),
+			"apiVersion": JSONSchemaProps("string", "", nil, nil),
+			"kind":       JSONSchemaProps("string", "", nil, nil),
+			"metadata":   JSONSchemaProps("object", "", nil, nil),
+			"spec": JSONSchemaProps("object", "", []string{"complex"}, map[string]extv1beta1.JSONSchemaProps{
+				"simple": JSONSchemaProps("integer", "int32", nil, nil),
+				"complex": JSONSchemaProps("object", "", nil, map[string]extv1beta1.JSONSchemaProps{
+					"simple_nested": JSONSchemaProps("integer", "int32", nil, nil),
+					"complex_nested": JSONSchemaProps("object", "", []string{"attribute"}, map[string]extv1beta1.JSONSchemaProps{
+						"attribute": JSONSchemaProps("string", "byte", nil, nil),
 					}),
 				}),
 			}),
-			"status": JSONSchemaProps("string", "", nil),
+			"status": JSONSchemaProps("string", "", nil, nil),
 		},
 	}
 }
