e6ef8073d3ee456b09c52d3ebf65ee29b2d516f4
Igor Sutton <igorsutton@me.com>
2019-12-29 22:05:21 +0100

    Add metadata field in schema

--- pkg/orchid/jsonschema/orchid.go
+++ pkg/orchid/jsonschema/orchid.go
@@ -8,6 +8,23 @@ func OrchidOpenAPIV3Schema() extv1.JSONSchemaProps {
 	properties := map[string]extv1.JSONSchemaProps{
 		"apiVersion": StringProp,
 		"kind":       StringProp,
+		"metadata": {
+			Type: Object,
+			Properties: map[string]extv1.JSONSchemaProps{
+				"labels": {
+					Type: Object,
+					AdditionalProperties: &extv1.JSONSchemaPropsOrBool{
+						Schema: &StringProp,
+					},
+				},
+				"annotations": {
+					Type: Object,
+					AdditionalProperties: &extv1.JSONSchemaPropsOrBool{
+						Schema: &StringProp,
+					},
+				},
+			},
+		},
 	}
 	return extv1.JSONSchemaProps{
 		Type:              Object,
